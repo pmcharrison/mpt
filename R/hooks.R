ce0498d9dc650536636eacd7d29caa0ac48272b9
Peter M C Harrison <pmc.harrison@gmail.com>
2018-09-26 09:54:23 +0100

    Updating date for package on-load hook

--- R/hooks.R
+++ R/hooks.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   packageStartupMessage(
-    "Note: on September 25th 2018, the following change was made to ",
+    "Note: on September 26th 2018, the following change was made to ",
     "default arguments for mpt(), demo_mpt(), and standalone_mpt():\n",
     "- next_item.estimator now defaults to 'BM', not 'WL'.\n"
   )
