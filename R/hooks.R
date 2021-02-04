ef66b4d685de1c3e994110d294fc504206cf82d0
Peter Harrison <pmc.harrison@gmail.com>
2021-02-04 17:38:19 +0000

    Remove old package startup message

--- R/hooks.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  packageStartupMessage(
-    "Note: on September 26th 2018, the following change was made to ",
-    "default arguments for mpt(), demo_mpt(), and standalone_mpt():\n",
-    "- next_item.estimator now defaults to 'BM', not 'WL'.\n"
-  )
-}
