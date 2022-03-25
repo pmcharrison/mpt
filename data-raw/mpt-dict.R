6a305024cbf9dbc7ddafae5ffb16d0b77baa6722
Peter Harrison <pmch2@cam.ac.uk>
2022-03-25 15:59:37 +0000

    Add UTF-8 encoding to dictionaries

--- data-raw/mpt-dict.R
+++ data-raw/mpt-dict.R
@@ -1,31 +1,31 @@
-input <- read.csv("data-raw/mpt-dict.csv", stringsAsFactors = FALSE)
+input <- read.csv("data-raw/mpt-dict.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(input)[[1]] <- "key"
 
-russian <- read.csv("data-raw/dict-russian.csv", stringsAsFactors = FALSE)
+russian <- read.csv("data-raw/dict-russian.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(russian)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == russian$key))
 input$RU <- russian$RU
 
-dutch <- read.csv("data-raw/dict-dutch.csv", stringsAsFactors = FALSE)
+dutch <- read.csv("data-raw/dict-dutch.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(dutch)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == dutch$key))
 input$NL <- dutch$NL
 
-italian <- read.csv("data-raw/dict-italian.csv", stringsAsFactors = FALSE)
+italian <- read.csv("data-raw/dict-italian.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(italian)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == italian$key))
 input$IT <- italian$IT
 
-spanish <- read.csv("data-raw/dict-spanish.csv", stringsAsFactors = FALSE)
+spanish <- read.csv("data-raw/dict-spanish.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(spanish)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == spanish$key))
 input$ES <- spanish$ES
 
-formal_german <- read.csv("data-raw/dict-de_formal.csv", stringsAsFactors = FALSE)
+formal_german <- read.csv("data-raw/dict-de_formal.csv", stringsAsFactors = FALSE, encoding = "utf-8")
 names(formal_german)[[1]] <- "key"
 stopifnot(!anyDuplicated(input$key),
           all(input$key == formal_german$key))
